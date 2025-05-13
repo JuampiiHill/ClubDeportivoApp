aec5b139d61e7469cd40e9734f0fb18404f0a862
JuanHillcoat <juanpablohillcoat@gmail.com>
2025-05-13 17:21:04 -0300

    se crean todas las funciones para redirigir a las pantallas

--- app/src/main/java/com/example/clubdeportivo/HomeActivity.kt
+++ app/src/main/java/com/example/clubdeportivo/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.clubdeportivo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,54 @@ class HomeActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnAddMember: LinearLayout = findViewById(R.id.btn_add_member)
+        btnAddMember.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnPay: LinearLayout = findViewById(R.id.btn_pay)
+        btnPay.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btListQuotas: LinearLayout = findViewById(R.id.btn_list_quotas)
+        btListQuotas.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnAddActivity: LinearLayout = findViewById(R.id.btn_add_activity)
+        btnAddActivity.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnNavHome: LinearLayout = findViewById(R.id.nav_btn_home)
+        btnNavHome.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnNavMembers: LinearLayout = findViewById(R.id.nav_btn_members)
+        btnNavMembers.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnNavActivity: LinearLayout = findViewById(R.id.nav_btn_activities)
+        btnNavActivity.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val btnNavPaymets: LinearLayout = findViewById(R.id.nav_btn_payments)
+        btnNavPaymets.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
